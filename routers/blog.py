813187ea950cf34ec7eb67ae56121c1f29111d8d
Alexandr Domarev <domarevsasha985@gmail.com>
2022-07-17 14:00:39 +0300

    project

---
+++ routers/blog.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter, Depends, status, HTTPException
+import schemas, models, oaut2
+from sqlalchemy.orm import Session
+from typing import List
+from database import get_db
+from repository import blog
+
+router = APIRouter(
+    prefix='/blog',
+    tags=['Blogs']
+)
+
+
+@router.get('/', status_code=201, response_model=List[schemas.ShowBlog])
+def all(db: Session = Depends(get_db),current_user:schemas.User = Depends(oaut2.get_current_user)):
+    return blog.get_all(db)
+
+
+@router.post('/', status_code=status.HTTP_201_CREATED)
+def create(request: schemas.Blog, db: Session = Depends(get_db),current_user:schemas.User = Depends(oaut2.get_current_user)):
+    return blog.create(request, db)
+
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id:int, db: Session = Depends(get_db),current_user:schemas.User = Depends(oaut2.get_current_user)):
+    return blog.destroy(id,db)
+
+
+@router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id:int, request: schemas.Blog, db: Session = Depends(get_db),current_user:schemas.User = Depends(oaut2.get_current_user)):
+    return blog.update(id, request,db)
+
+
+@router.get('/{id}', status_code=200, response_model=schemas.ShowBlog)
+def show(id:int, db: Session = Depends(get_db),current_user:schemas.User = Depends(oaut2.get_current_user)):
+    return blog.show(id,db)
